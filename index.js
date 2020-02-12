396da332e66b7640e93be2fb19e060d841fea059
Salvador Aceves <salvador@aceves.mx>
2020-02-11 19:36:09 -0600

    update to run as a depedency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const AWS = require('aws-sdk');
+const path = require('path');
 
 const { env: {
   region = 'us-east-1',
@@ -32,9 +33,17 @@ client.getSecretValue({SecretId: secretName}, function(err, data) {
         }
     }
   const args = process.argv.slice();
-  const indexFound = args.findIndex(el => el.indexOf('secrets2env') > -1)
+  const indexFound = (() => {
+    let retVal = args.findIndex(el => el.indexOf('secrets2env') > -1)
+    if (retVal > -1) return retVal;
+    return args.findIndex(el => el.indexOf('loadSecrets') > -1)
+  })();
   if (indexFound < args.length - 1) {
     process.argv.splice(indexFound, 1);// gosth the script
-    require(args[indexFound + 1]);
+    if (args[indexFound].indexOf('node_modules/') > -1) { // we are running it from bin
+      require(`${args[indexFound].split('node_modules/')[0]}${args[indexFound + 1]}`);
+    } else { // locally run
+      require(args[indexFound + 1]);
+    }
   }
 });
