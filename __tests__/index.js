62c494c9b43de899ad56584e104424b4e9e0c528
Salvador Aceves <salvador@aceves.mx>
2020-02-13 11:40:30 -0600

    mock aws secrets call

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,14 @@
 const lib = require('../index');
 const { exec } = require('child_process');
 const fs = require('fs');
+const { SecretsManager } = require('aws-sdk');
+const { on, infer } = require('aws-promise-jest-mock');
+
+jest.mock('aws-sdk');
+
+const secretFixture = {
+  SecretString: JSON.stringify({ demoSecret: 'demoValue' }),
+};
 
 async function sh(cmd) {
   return new Promise(function (resolve, reject) {
@@ -18,17 +26,29 @@ async function sh(cmd) {
 // with the value demoValue
 describe('required test', () => {
   it('should be able to required the library', async () => {
+    const m = on(SecretsManager)
+      .mock('getSecretValue', infer)
+      .resolve(secretFixture);
     const retVal = await lib.retrieve();
+    expect(m.mock).toHaveBeenCalled();
     expect(retVal.demoSecret).toBe('demoValue');
   });
-  it('should crate a file', async () => {
+  it('should create a file', async () => {
+    const m = on(SecretsManager)
+      .mock('getSecretValue', infer)
+      .resolve(secretFixture);
     const envFile = `${__dirname}/../.env`;
     if (fs.existsSync(envFile)) fs.unlinkSync(envFile);
     await sh(`${__dirname}/../bin.js`);
+    expect(m.mock).toHaveBeenCalled();
     expect(fs.existsSync(envFile)).toBe(true);
   });
   it('should create the env variables', async () => {
+    const m = on(SecretsManager)
+      .mock('getSecretValue', infer)
+      .resolve(secretFixture);
     await lib.config();
+    expect(m.mock).toHaveBeenCalled();
     expect(process.env.demoSecret).toBe('demoValue');
   });
 });
